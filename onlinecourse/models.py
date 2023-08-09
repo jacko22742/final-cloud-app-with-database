27856314a14f79f343756eae9a68eece8e55eae1
Jack O'Sullivan <jacko22742@live.ie>
2023-08-09 07:48:15 -0400

    Updates for IBM Course

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,6 +94,29 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question_text = models.TextField(default="")
+    grade = models.IntegerField(default=1)
+
+    def is_get_score(self, selected_ids):
+         all_answers = self.choice_set.filter(is_correct=True).count()
+         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+         if all_answers == selected_correct:
+             return True
+         else:
+             return False
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.TextField(default="")
+    is_correct = models.BooleanField(default=False)
+
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
